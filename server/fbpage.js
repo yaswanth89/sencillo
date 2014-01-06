94f1a45f5fea071b838dcddbeca5706f0ac9e131
Yaswanth Y <y.yaswanth89@gmail.com>
2014-01-06 17:58:44 +0530

    npm module changes

--- server/fbpage.js
+++ server/fbpage.js
@@ -1,4 +1,4 @@
-var connect = Meteor.require('connect');
+var connect = Npm.require('connect');
 var base64url = Meteor.require('base64url');
 var Fiber = Npm.require('fibers');
 var app = WebApp.connectHandlers;
